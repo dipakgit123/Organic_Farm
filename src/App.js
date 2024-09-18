041aac8b8364190f82b65bdcc5c606628855933e
pranavapawar <pranavapawar12@gmail.com>
2024-09-18 18:48:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b51041af26063586a544e5283eb0715e777e8537
 // import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/SignIn';
@@ -14,12 +7,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
 
-=======
->>>>>>> d3fcf0eabb759ee3981bfc4066d5138339cb5067
 import './App.css';
 import Contact from './components/Contact';
 import Home from './components/Home';
->>>>>>> 71ec946a45c2c47d46662f1b9d734aae320d8714
 
 import Services from './components/Services';
 
@@ -39,23 +29,18 @@ function App() {
   ]);
 
   return (
-<<<<<<< HEAD
-    <div>
+    <>
     <Services/>
+    <Contact/>
     
-=======
-<<<<<<< HEAD
     <div className='App'>
    <RouterProvider router={route}></RouterProvider>
->>>>>>> b51041af26063586a544e5283eb0715e777e8537
     </div>
 
     
-=======
-    <>
+    
     <Contact/>
     </>
->>>>>>> 71ec946a45c2c47d46662f1b9d734aae320d8714
   );
 }
 
